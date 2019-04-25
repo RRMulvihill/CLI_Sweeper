5b225510ba2cb1cbb16cd0ddf456f930defb6a22
Riley Mulvihill <Friskog55@GMail.com>
2019-04-24 22:15:41 -0400

    fixed flag issue

--- Tile.py
+++ Tile.py
@@ -12,11 +12,11 @@ class Tile:
         print("X:" + str(self.x) + " Y:" + str(self.y) + " Mine:" + str(self.has_mine))
 
     def print_tile(self):
-        if self.popped == True:
+        if self.flagged == True:
+                return "!"
+        elif self.popped == True:
             if self.has_mine == True:
                 return "X"
-            elif self.flagged == True:
-                return "!"
             else:
                 return str(self.adjacet_mines)
 
